2bd8c42678b5bc73821e5e4ced16f1dec1b36c6e
dg1223 <alavi1223@hotmail.com>
2023-03-25 01:17:21 -0400

    call event handler using arrow function

--- src/components/Cosmetic/Cosmetic.jsx
+++ src/components/Cosmetic/Cosmetic.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import "./Cosmetic.css";
 
 const Cosmetic = (props) => {
   const { name, price, id } = props.cosmetic;
+
+  const addToCart = (id) => {
+    console.log("item added", id);
+  };
+
+  const addToCartWithParam = () => addToCart(id);
+
   return (
-    <div>
+    <div className="product">
       <h2>Buy this:{name} </h2>
       <p>Only for: {price}</p>
       <p>
         <small>It has id: {id}</small>
       </p>
+      <button onClick={addToCartWithParam}>Add to cart</button>
+      <button onClick={() => addToCart(id)}>Add to cart: Shortcut</button>
     </div>
   );
 };
