ef15d8247dfa06935eea542f4d2849f974fb12f6
dg1223 <alavi1223@hotmail.com>
2023-03-25 00:31:08 -0400

    export import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Cosmetics from "./components/Cosmetics/Cosmetics";
+import Shoes from "./components/Shoes/Shoes";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Cosmetics></Cosmetics>
+      <Shoes></Shoes>
     </div>
   );
 }
